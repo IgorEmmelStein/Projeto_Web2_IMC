25f0f134299b9d3bba7cc5b41f9aa6320ac6160f
Igor Stein <igorestein@gmail.com>
2026-03-29 20:01:47 -0300

    ajuste telas de consulta

--- php/dadosConexao.php
+++ php/dadosConexao.php
@@ -3,4 +3,14 @@ $localSevidor = "localhost";
 $usuario = "root";
 $senha = "";
 $nomeBaseDados = "projeto_web2_imc";
-?>
+
+include __DIR__ . "/funcoes.php"; // Garante o caminho correto
+
+if (isset($_GET['acao'])) {
+    $conexao = conectar();
+    $filtro = $_GET['acao'];
+
+    consultaEstudantes($conexao, $filtro);
+
+    mysqli_close($conexao);
+}
