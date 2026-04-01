20b6044d5f31f7856b973620dc72bb0c9f7474e1
JoaoPierret <joaop.pierret@gmail.com>
2026-04-01 20:35:12 -0300

    Inserção dos dados por meio do forms automatizada

--- php/dadosForm.php
+++ php/dadosForm.php
@@ -1,4 +1,15 @@
 <?php
     include'funcoes.php';
-    
+    include'dadosConexao.php';
+
+    $nomeRecebido = $_POST['containerNome'];
+    $sobrenomeRecebido = $_POST['containerSobrenome'];
+    $idadeRecebida = $_POST['containerIdade'];
+    $pesoRecebido = $_POST['containerPeso'];
+    $alturaRecebida = $_POST['containerAltura'];
+
+    $queryInsercao = "INSERT INTO estudantes (nome, sobrenome, idade, peso, altura) VALUES (?, ?, ?, ?, ?)";
+
+    $insercaoDosDados = $preparar->prepare($queryInsercao);
+    $insercaoDosDados->execute([$nomeRecebido, $sobrenomeRecebido, $idadeRecebida, $pesoRecebido, $alturaRecebida]);
 ?>
